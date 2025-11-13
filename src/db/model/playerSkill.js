fd5bd8e927c053686a91f51392c1de7c752d838b
fajarsetiawan17 <fajar.setiawan17121994@gmail.com>
2025-11-13 18:19:25 +0700

    Add files via upload

---
+++ src/db/model/playerSkill.js
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------------------------------------
+// YOU CAN MODIFY THE CODE BELOW IN ORDER TO COMPLETE THE TASK
+// YOU SHOULD NOT CHANGE THE EXPORTED VALUE OF THIS FILE
+// YOU SHOULD NOT CHANGE THE DATABASE STRUCTURE, ADDING NEW FIELDS, RENAMING OR REMOVING THE CURRENT FIELDS MAY RESULT IN A FAILED TEST
+// ---------------------------------------------------------------------------------------------
+
+import Sequelize from 'sequelize';
+import database from '../index';
+
+const PlayerSkill = database.define('playerSkill', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    skill: {
+        type: Sequelize.STRING(200),
+    },
+    value: {
+        type: Sequelize.INTEGER
+    }
+}, {
+    timestamps: false
+})
+
+PlayerSkill.associate = (models) => {
+    models.PlayerSkill.belongsTo(models.Player);
+}
+
+export default PlayerSkill;
